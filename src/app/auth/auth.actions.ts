fd60d457f545b6f8170ea36302d5bbbd6f6c0120
nidhi <kumarinidhi0594@gmail.com>
2025-09-06 15:53:46 +0200

    Action-ttype created

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -5,4 +5,8 @@ import { User } from "./model/user.model";
 export const login = createAction(
     "[Login Page] User Login",
     props<{user: User}>()
-)
+);
+
+export const logout = createAction(
+    "[Top Menu] Logout"
+);
